bd27ba435dfcf2a6e330bfce3a4bff4f9c6a2f33
Bintang Fajar Julio <79626613+bintangfjulio@users.noreply.github.com>
2024-09-30 12:18:35 +0700

    Update bubble_sort.js

--- bubble_sort.js
+++ bubble_sort.js
@@ -1,16 +1,20 @@
-function bubbleSort(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length - i - 1; j++) {
-      if (arr[j] < arr[j + 1]) {
-        let temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
+function sorting(arr) {
+  var swap;
+
+  do {
+    swap = false;
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        let temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+
+        swap = true;
       }
     }
-  }
+  } while (swap);
+
   console.log(arr);
 }
 
-bubbleSort([
-  1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 53,
-]);
+sorting([4, 1, 2, 7, 8, 2, 10, 9]);
